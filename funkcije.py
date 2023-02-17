bf447ca3d8102f8129bc719d86db27a6e6e518e5
Dado Skrgic <dadoskrgic@gmail.com>
2023-02-17 23:23:36 +0100

    .

--- funkcije.py
+++ funkcije.py
@@ -1,16 +1,29 @@
 import matplotlib.pyplot as plt
 import librosa.display
 import numpy as np
-def create_spectrogram(audio_file, image_file):
-    fig = plt.figure()
-    ax = fig.add_subplot(1, 1, 1)
-    fig.subplots_adjust(left=0, right=1, bottom=0, top=1)
+from librosa import get_duration
+
+def create_spectrogram(audio_file, image_name, image_format):
 
     y, sr = librosa.load(audio_file)
-    ms = librosa.feature.melspectrogram(y = y, sr = sr)
-    log_ms = librosa.power_to_db(ms, ref=np.max)
-    librosa.display.specshow(log_ms, sr=sr)
+    n = 1
+    if get_duration(y = y, sr = sr) > 6:
+        n = int(get_duration(y = y, sr = sr) // 3)
 
-    fig.savefig(image_file)
-    plt.close(fig)
+    step = int(sr * get_duration(y = y, sr = sr) // n)
+    print(step, n)
+    
+    for i in range(n):
+        print(i * step , (i + 1) * step)
+        fig = plt.figure()
+        ax = fig.add_subplot(1, 1, 1)
+        fig.subplots_adjust(left=0, right=1, bottom=0, top=1)
+        ms = librosa.feature.melspectrogram(y = y[i * step : (i + 1) * step], sr = sr)
+        log_ms = librosa.power_to_db(ms, ref=np.max)
+        librosa.display.specshow(log_ms, sr=sr)
+
+        fig.savefig(image_name + str(i) + image_format)
+        plt.close(fig)
+
+    return n
     
--- main.py
+++ main.py
@@ -8,6 +8,8 @@ from funkcije import create_spectrogram
 from keras.models import load_model
 import matplotlib.pyplot as plt
 import numpy as np
+import keras.losses
+
 
 
 INSTRUMENTS = ["cel", "cla", "flu", "gac", "gel", "org", "pia", "sax", "tru", "vio", "voi"]
@@ -33,20 +35,23 @@ async def upload_file(file: UploadFile):
     audio_file = io.BytesIO(audio_bytes)
 
     # print(1)
-
-    create_spectrogram(audio_file, 'temp.png')
+    n = create_spectrogram(audio_file, 'temp', '.png')
 
     # print(2)
-
-    x = np.array([image.img_to_array(image.load_img('temp.png', target_size=(224, 224, 3)))])
-
-    # print(np.shape(x))  
-    prediction = AI_MODEL.predict(x)
-    # print(np.shape(prediction))
-    ans = {}
-    for i in range(len(INSTRUMENTS)):
-        if prediction[0][i] > 0.3: ans[INSTRUMENTS[i]] = 1
-        else: ans[INSTRUMENTS[i]] = 0
+    ans = {inst: 0 for inst in INSTRUMENTS}
+    # print(ans.keys())
+    for i in range(n):
+        x = np.array([image.img_to_array(image.load_img('temp{}.png'.format(i), target_size=(224, 224, 3)))])
+        # print(x)
+        # print(np.shape(x))  
+        prediction = AI_MODEL.predict(x)
+        # print(np.shape(prediction))
+        # print(prediction)
+        # ans = {}
+        for i in range(len(INSTRUMENTS)):
+            if prediction[0][i] > 0.4: ans[INSTRUMENTS[i]] += 1
+            # else: ans[INSTRUMENTS[i]] = 0
+        # print(ans)
     return ans
 
     y, sr = librosa.load(audio_file)
